be46e838a10ee730d32ae7dc26d862f7b4a578f9
Ashish Sharma <ashish@algorythma.com>
2017-10-29 13:15:43 +0530

    test with commented out models - to rollback

--- model/mongo.js
+++ model/mongo.js
@@ -1,16 +1,16 @@
-var mongoose    =   require("mongoose");
-mongoose.connect('mongodb://localhost:27017/demoDb');
-// create instance of Schema
-var mongoSchema =   mongoose.Schema;
+// var mongoose    =   require("mongoose");
+// mongoose.connect('mongodb://localhost:27017/demoDb');
+// // create instance of Schema
+// var mongoSchema =   mongoose.Schema;
 
-var miRNASchema = {
-    "miRNA_name":String,
-    "cancer":String,
-    "cell_line":String,
-    "targetD":String,
-    "technique":String,
-    "functionName":String,
-    "link_addr":String
-};
+// var miRNASchema = {
+//     "miRNA_name":String,
+//     "cancer":String,
+//     "cell_line":String,
+//     "targetD":String,
+//     "technique":String,
+//     "functionName":String,
+//     "link_addr":String
+// };
 
-module.exports = mongoose.model('miRNAData',miRNASchema);
+// module.exports = mongoose.model('miRNAData',miRNASchema);
